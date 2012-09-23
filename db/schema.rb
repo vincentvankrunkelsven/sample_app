b6a25981e4563d0f1467d6b16c22a78c37eb8dd9
Vincent Vankrunkelsven <v.vankrunkelsven@gmail.com>
2012-09-23 15:54:57 +0200

    Update postrgrsql

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120829015124) do
-
-  create_table "users", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
+ActiveRecord::Schema.define(:version => 20120923132924) do
 
 end
